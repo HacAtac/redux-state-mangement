c6ea2535e080e1dfadfaee8b71dfee822caa3416
HacAtac <jhack00@icloud.com>
2022-01-31 07:52:38 -0600

    nav bar completed

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import { useQuery } from '@apollo/client';
+
+// import {  } from '../utils/queries';
+
+const Home = () => {
+  //   const { loading, data } = useQuery(QUERY_THOUGHTS);
+  //   const thoughts = data?.thoughts || [];
+
+  return <main></main>;
+};
+
+export default Home;
