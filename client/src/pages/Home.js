0169069571d35437617c9ef843e5ab1c93c82f71
HacAtac <jhack00@icloud.com>
2022-01-31 08:00:08 -0600

    started home page, no styles yet

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,10 +4,20 @@ import React from "react";
 // import {  } from '../utils/queries';
 
 const Home = () => {
-  //   const { loading, data } = useQuery(QUERY_THOUGHTS);
-  //   const thoughts = data?.thoughts || [];
+  return (
+    <main>
+      <h1>Welcome to the CRUD App with GraphQL and Redux</h1>
+      <p>
+        This is a simple MERN stack app that will be used to do CRUD methods on
+        Users.
+      </p>
 
-  return <main></main>;
+      <p>
+        I'm also showcasing conditional rendering based off of Authentication
+        with JWT's.
+      </p>
+    </main>
+  );
 };
 
 export default Home;
